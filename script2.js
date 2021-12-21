253057bdba4cfb21bc2ff73545d7f6300192a460
piapiasop <96457518+piapiasop@users.noreply.github.com>
2021-12-21 10:28:44 +0700

    Add files via upload
    upload project

---
+++ script2.js
@@ -0,0 +1,40 @@
+const logo= document.getElementById ('logo');
+const nama= document.getElementById ('nama');
+
+function rpl() {
+				logo.src="rpl.png";
+				nama.innerHTML="Rekayasa Perangkat Lunak";
+}
+function tkj() {
+	logo.src="tkj.png";
+	nama.innerHTML="Teknik Komputer & Jaringan";
+}
+function mm() {
+	logo.src="mm.png";
+	nama.innerHTML="Multimedia";
+}
+function tbsm() {
+	logo.src="tbsm.png";
+	nama.innerHTML="Teknik & Bisnis Sepeda Motor";
+}
+function Sukapura() {
+				logo.src="Sukapura.png";
+				
+}
+
+function akl() {
+	logo.src="akl.png";
+	nama.innerHTML="Akuntansi";
+}
+function otkp() {
+	logo.src="otkp.png";
+	nama.innerHTML="Otomatisasi Tata Kelola Perkantoran";
+}
+function tabog() {
+	logo.src="tabog.png";
+	nama.innerHTML="Tata Boga";
+}
+function tb() {
+	logo.src="tb.png";
+	nama.innerHTML="Tata Busana";
+}
